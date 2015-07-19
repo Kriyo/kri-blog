02c81937afe7c6087f37e7a71dea794ffc9dff3b
Kriyo <krikenn@gmail.com>
2015-07-19 23:38:20 +0100

    more stuff...

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -80,4 +80,22 @@ angular.module('ngkriApp')
     $scope.$on('$destroy', function(){
       socket.unsyncUpdates('blogPost');
     });
+
+    $scope.carouselSlider = function($scope){
+    $scope.myInterval = 3000;
+    $scope.slides = [
+      {
+        image: 'http://lorempixel.com/400/200/'
+      },
+      {
+        image: 'http://lorempixel.com/400/200/food'
+      },
+      {
+        image: 'http://lorempixel.com/400/200/sports'
+      },
+      {
+        image: 'http://lorempixel.com/400/200/people'
+      }
+    ];
+  }
   });
